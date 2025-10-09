a6d7e895fba654850db00b3387a15cf2cefc4d45
vitormneris <joaomoreiraneris0@gmail.com>
2025-10-09 11:50:26 -0300

    Updates

--- _next/static/chunks/19647a2ea270e4ba.js
+++
@@ -1,5 +0,0 @@
-__turbopack_load_page_chunks__("/_error", [
-  "static/chunks/17722e3ac4e00587.js",
-  "static/chunks/0c7f45e51d077afe.js",
-  "static/chunks/turbopack-4b2369e11e6eb560.js"
-])
--- _next/static/chunks/f60e18978f81fdc3.js
+++
@@ -1,5 +0,0 @@
-__turbopack_load_page_chunks__("/_app", [
-  "static/chunks/e60ef129113f6e24.js",
-  "static/chunks/0c7f45e51d077afe.js",
-  "static/chunks/turbopack-3b8146ba033008ad.js"
-])
